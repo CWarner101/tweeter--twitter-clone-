5506b6be6fbc3a2160db8f46569ea1531f23168c
CWarner101 <con.war10@gmail.com>
2024-03-18 14:32:53 -0400

    Create password reset templates with bootstrap styling. Add email verifiction method.

--- django_project/settings.py
+++ django_project/settings.py
@@ -115,7 +115,7 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
@@ -136,6 +136,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Login and Logout redirect URLs
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+# Email Settings
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+DEFAULT_FROM_EMAIL = "noreply@localhost"
+EMAIL_HOST = "barnesbrothers.net"
+EMAIL_HOST_USER	= ""
+EMAIL_HOST_PASSWORD	= ""
+EMAIL_PORT = 2500
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = False
 
 # Crispy Forms Settings
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
