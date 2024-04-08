6930386477e001ec73e194062f64540d35a4a793
CWarner101 <con.war10@gmail.com>
2024-04-08 17:32:42 -0400

    Fix public profile view to work off of current user object instead of current logged in user.

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from .views import SignUpView, ProfileUpdateView, ProfilePublicView
 
 urlpatterns = [
     path("profile/<int:pk>/", ProfileUpdateView.as_view(), name="profile_update"),
-    path("profile/public/<int:pk>/", ProfilePublicView.as_view(), name="profile_public"),
+    path("profile_public/<int:pk>/", ProfilePublicView.as_view(), name="profile_public"),
     path("signup/", SignUpView.as_view(), name="signup"),
 ]
